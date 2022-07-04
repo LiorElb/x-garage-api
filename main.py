7fa24d4a4ae60ad34921194485843c3f090dde61
liorelbaz <liorelbaz@microsoft.com>
2022-07-04 18:46:12 +0300

    update assert function

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import os
 from http import HTTPStatus
+from typing import List, Iterable
 
-from fastapi import FastAPI, Response
+from fastapi import FastAPI, Response, HTTPException
 import motor.motor_asyncio as motor
-
 
 from customer import Customer
 
@@ -19,10 +19,8 @@ async def get_customers():
 
 
 @app.post("/customers")
-async def add_customer(customer: Customer, response: Response):
-    if await CUSTOMERS.find_one({"license_plate_numbers": {"$in": customer.license_plate_numbers}}) is not None:
-        response.status_code = HTTPStatus.CONFLICT
-        return {"message": "error - license number already exists"}
+async def add_customer(customer: Customer):
+    await assert_license_numbers_dont_already_exist(customer.license_plate_numbers)
 
     await CUSTOMERS.insert_one(customer.dict())
     return {"message": "success"}
@@ -35,6 +33,10 @@ async def get_customer(license_plate_number: str):
 
 @app.put("/customers/{license_plate_number}")
 async def update_customer(license_plate_number: str, customer: Customer):
+    existing = await CUSTOMERS.find_one({"license_plate_numbers": license_plate_number})
+    await assert_license_numbers_dont_already_exist(customer.license_plate_numbers,
+                                                    allow=existing['license_plate_numbers'])
+
     await CUSTOMERS.replace_one({"license_plate_numbers": license_plate_number}, customer.dict())
     return {"message": "success"}
 
@@ -69,3 +71,15 @@ async def remove_car(license_plate_number: str, plate_number_to_delete: str):
         update={"$pull": {"license_plate_numbers": plate_number_to_delete}}
     )
     return {"message": "success"}
+
+
+async def assert_license_numbers_dont_already_exist(license_plate_numbers: List[str], allow: Iterable[str] = ()):
+    existing = await CUSTOMERS.find_one({"license_plate_numbers": {"$in": license_plate_numbers}})
+
+    if existing is None:
+        return
+
+    if any(plate_num in existing['license_plate_numbers'] for plate_num in allow):
+        return
+
+    raise HTTPException(status_code=HTTPStatus.CONFLICT, detail="license number already exists")
