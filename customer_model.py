4aaada24bd71e291b565de2a64f35cbc111d1339
liorelbaz <liorelbaz@microsoft.com>
2022-07-10 09:47:55 +0300

    make email optional

--- customer_model.py
+++ customer_model.py
@@ -10,9 +10,9 @@ class CustomerModel(BaseModel):
     id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
     cars: list[str] = Field(default_factory=list)
     name: str = Field(...)
-    email: Optional[EmailStr] = Field(...)
     phone_number: str = Field(...)
-    address: Optional[str] = Field(...)
+    email: Optional[EmailStr] = Field(default=None)
+    address: Optional[str] = Field(default=None)
 
     class Config:
         allow_population_by_field_name = True
