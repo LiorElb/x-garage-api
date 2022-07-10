aabc65442ae71bf2d3c20ffe315156fd730e63db
liorelbaz <liorelbaz@microsoft.com>
2022-07-10 11:46:14 +0300

    assert mongo env variables

--- mongo_client.py
+++ mongo_client.py
@@ -1,8 +1,12 @@
 import os
 import motor.motor_asyncio as motor
 
-MONGO_HOST = os.getenv('GARAGE_MONGO_HOST', 'localhost')
-MONGO_PORT = os.getenv('GARAGE_MONGO_PORT', '27017')
+MONGO_HOST = os.getenv('GARAGE_MONGO_HOST')
+MONGO_PORT = os.getenv('GARAGE_MONGO_PORT')
+
+if not all((MONGO_HOST, MONGO_PORT)):
+    raise EnvironmentError("Please define MONGO_HOST and MONGO_PORT in env")
+
 client = motor.AsyncIOMotorClient(f'mongodb://{MONGO_HOST}:{MONGO_PORT}/')
 CUSTOMERS = client['main']['customers']
 CARS = client['main']['cars']
