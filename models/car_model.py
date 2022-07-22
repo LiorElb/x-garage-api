908c479c2b4e32f18db697531c90aaf5c7977269
liorelbaz <liorelbaz@microsoft.com>
2022-07-22 14:46:54 +0300

    update car model

--- models/car_model.py
+++ models/car_model.py
@@ -9,17 +9,17 @@ class CarModel(BaseModel):
     id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
     license_plate_number: str = Field(...)
     code: str | None = Field(default=None)
-    tozeret_nm: str | None = Field(default=None)  # יצרן
-    shnat_yitzur: int | None = Field(default=None)  # 2010
-    degem_manoa: str | None = Field(default=None)  # "665 וברוט"
-    tokef_dt: str | None = Field(default=None)  # 2023-06-21T00:00:00
-    baalut: str | None = Field(default=None)  # "פרטי"
-    misgeret: str | None = Field(default=None)  # "KPTN0B1FSAP059254"
-    tzeva_rechev: str | None = Field(default=None)  # "אפור"
-    zmig_kidmi: str | None = Field(default=None)  # "215/65R16"
-    zmig_ahori: str | None = Field(default=None)  # "215/65R16"
-    sug_delek_nm: str | None = Field(default=None)  # "דיזל"
-    kinuy_mishari: str | None = Field(default=None)  # "RODIUS"
+    tozeret_nm: str | None = Field(default=None)
+    shnat_yitzur: int | None = Field(default=None)
+    degem_manoa: str | None = Field(default=None)
+    tokef_dt: str | None = Field(default=None)
+    baalut: str | None = Field(default=None)
+    misgeret: str | None = Field(default=None)
+    tzeva_rechev: str | None = Field(default=None)
+    zmig_kidmi: str | None = Field(default=None)
+    zmig_ahori: str | None = Field(default=None)
+    sug_delek_nm: str | None = Field(default=None)
+    kinuy_mishari: str | None = Field(default=None)
 
     class Config:
         allow_population_by_field_name = True
@@ -29,10 +29,20 @@ class CarModel(BaseModel):
         }
         schema_extra = {
             "example": {
-                "name": "John Doe",
-                "email": "jdoe@example.com",
-                "phone_number": "054-3532312",
-                "address": "123 Street, NYC",
+                "license_plate_number": "11-111-11",
+                "code": "*1234",
+                "mispar_rechev": 2117772,
+                "tozeret_nm": "סאנגיונג ד.קור",
+                "shnat_yitzur": 2010,
+                "degem_manoa": "665 וברוט",
+                "tokef_dt": "2023-06-21T00:00:00",
+                "baalut": "פרטי",
+                "misgeret": "KPTN0B1FSAP059254",
+                "tzeva_rechev": "אפור",
+                "zmig_kidmi": "215/65R16",
+                "zmig_ahori": "215/65R16",
+                "sug_delek_nm": "דיזל",
+                "kinuy_mishari": "RODIUS",
             }
         }
 
@@ -48,21 +58,7 @@ class UpdateCarModel(BaseUpdateModel):
         }
         schema_extra = {
             "example": {
-                "name": "John Doe",
-                "email": "jdoe@example.com",
-                "phone_number": "054-3532312",
-                "address": "123 Street, NYC",
-                "mispar_rechev": 2117772,
-                "tozeret_nm": "סאנגיונג ד.קור",
-                "shnat_yitzur": 2010,
-                "degem_manoa": "665 וברוט",
-                "tokef_dt": "2023-06-21T00:00:00",
-                "baalut": "פרטי",
-                "misgeret": "KPTN0B1FSAP059254",
-                "tzeva_rechev": "אפור",
-                "zmig_kidmi": "215/65R16",
-                "zmig_ahori": "215/65R16",
-                "sug_delek_nm": "דיזל",
-                "kinuy_mishari": "RODIUS",
+                "license_plate_number": "11-111-11",
+                "code": "*3234",
             }
         }
