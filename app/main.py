8e9c89cda2c1a315ddd009c7d932c23f65914444
fradkintal <fradkintal@gmail.com>
2022-11-27 23:56:16 +0200

    Revert "Update main.py"
    This reverts commit 6c8030b7e56bfcb64760d006b23586a28dfcd0ea.

--- app/main.py
+++ app/main.py
@@ -167,9 +167,11 @@ async def get_cars_by_id(customer_id) -> list[str]:
 async def get_cars():
     return await CARS.find().to_list(length=None)
 
+
 @app.get("/cars/types", response_model=list[str | None], tags=['cars'])
 async def get_car_types(extended: bool = False):
-    return await CARS.distinct(f'government_data.{"tozar"}'+f' , government_data.{"kinuy_mishari"}'+f' , government_data.{"shnat_yitzur"}')
+    return await CARS.distinct(f'government_data.{"tozeret_nm" if extended else "tozar"}')
+
 
 @app.post("/cars", response_model=CarModel, status_code=HTTPStatus.CREATED, tags=['cars'])
 async def add_car(car: CarModel, bg_tasks: BackgroundTasks):
