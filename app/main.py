9cffa00a5956167ab0bad15c67c689380f1ec3f9
fradkintal <fradkintal@gmail.com>
2022-11-28 17:03:29 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -198,7 +198,7 @@ async def get_car_types1():
 
 @app.get("/cars/types2", tags=['cars'])
 async def get_car_types2():
-    results = await CARS.distinct[("government_data.shnat_yitzur"),("government_data.kinuy_mishari"),("government_data.tozar")]
+    results = await CARS.distinct(["government_data.shnat_yitzur","government_data.kinuy_mishari","government_data.tozar"])
     return results
 
 # @app.get("/cars/typesnew1", tags=['cars'])
