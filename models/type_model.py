6e1075d74a16b543cd27ddce94bd4ad735d28cd2
fradkintal <fradkintal@gmail.com>
2022-11-29 22:15:15 +0200

    Revert "Update type_model.py"
    This reverts commit 5ee1624f51dfd5492d97b621061465361b2d1cd7.

--- models/type_model.py
+++ models/type_model.py
@@ -8,7 +8,7 @@ from models.pyobjectid import PyObjectId
 class TypeModel(BaseModel):
 
     id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
-    list: list[str] = Field(...)
+    list: list[str] | None = Field(default=None)
     number1: int = Field(default=None)
     number2: int = Field(default=None)
     number3: int = Field(default=None)
