ada4cce808470f40f29919b647a53f2ae6697d79
fradkintal <fradkintal@gmail.com>
2022-11-29 22:15:47 +0200

    Revert "tpye model"
    This reverts commit 7506edd7f35258be0d75d8eaf0885b4aa7f7d3fe.

--- app/mongo_client.py
+++ app/mongo_client.py
@@ -10,7 +10,6 @@ client = motor.AsyncIOMotorClient(MONGO_CONNECTION_STR)
 CUSTOMERS = client['main']['customers']
 SUPPLIER = client['main']['supplier']
 CARS = client['main']['cars']
-Type = client['main']['type']
 Storage = client['main']['storage']
 Used = client['main']['used']
 Tipul = client['main']['tipul']
