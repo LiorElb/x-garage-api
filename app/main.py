4ee28ff3eb787e6c68389007dc26a31bb00d9752
fradkintal <fradkintal@gmail.com>
2022-12-07 23:15:11 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -187,27 +187,32 @@ async def get_car_type(car_num: str):
 async def get_car_types(extended: bool = False):
     return await CARS.distinct(f'government_data.{"tozeret_nm" if extended else "tozar"}')
 
-
-@app.get("/cars/types1", response_model=list[CarModel], tags=['cars'])
-async def get_cars1():
+@app.get("/cars/types0", response_model=list[CarModel], tags=['cars'])
+async def get_cars0():
     cars = await CARS.find().to_list(length=None)
     for car in cars:
-        x = car["government_data"]["tozar"]
-        y = car["government_data"]["kinuy_mishari"]
-        z = car["government_data"]["shnat_yitzur"]
-        return {x, y, z}
+        return car
+
+# @app.get("/cars/types1", response_model=list[CarModel], tags=['cars'])
+# async def get_cars1():
+#     cars = await CARS.find().to_list(length=None)
+#     for car in cars:
+#         x = car["government_data"]["tozar"]
+#         y = car["government_data"]["kinuy_mishari"]
+#         z = car["government_data"]["shnat_yitzur"]
+#         return {x, y, z}
 
 
-@app.get("/cars/types2", response_model=list[CarModel], tags=['cars'])
-async def get_cars2():
-    cars = await CARS.find().to_list(length=None)
-    list = []
-    for car in cars:
-        x = car["government_data"]["tozar"]
-        y = car["government_data"]["kinuy_mishari"]
-        z = car["government_data"]["shnat_yitzur"]
-        list.append({x, y, z})
-    return list
+# @app.get("/cars/types2", response_model=list[CarModel], tags=['cars'])
+# async def get_cars2():
+#     cars = await CARS.find().to_list(length=None)
+#     list = []
+#     for car in cars:
+#         x = car["government_data"]["tozar"]
+#         y = car["government_data"]["kinuy_mishari"]
+#         z = car["government_data"]["shnat_yitzur"]
+#         list.append({x, y, z})
+#     return list
 
 
 # async def get_car_types():
