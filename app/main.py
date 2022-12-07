89c26994026b082e619d402a55a71824ba65ca67
fradkintal <fradkintal@gmail.com>
2022-12-07 23:36:46 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -187,11 +187,13 @@ async def get_car_type(car_num: str):
 async def get_car_types(extended: bool = False):
     return await CARS.distinct(f'government_data.{"tozeret_nm" if extended else "tozar"}')
 
+
 @app.get("/cars/types0", tags=['cars'])
 async def get_cars0():
     cars = await CARS.find().to_list(length=None)
     for car in cars:
         return car
+
 
 @app.get("/cars/types1", tags=['cars'])
 async def get_cars1():
@@ -211,8 +213,17 @@ async def get_cars2():
         x = car["government_data"]["tozar"]
         y = car["government_data"]["kinuy_mishari"]
         z = car["government_data"]["shnat_yitzur"]
-        list.append(x, y, z)
+        list.append((x, y, z))
     return list
+    
+    
+    
+    # new_list = []
+    # for one_student_choice in list:
+    #     if one_student_choice not in new_list:
+    #         new_list.append(one_student_choice) 
+    # return new_list
+
 
 @app.get("/cars/types3", tags=['cars'])
 async def get_cars3():
