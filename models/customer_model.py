b7c96434df8d90aed2ab6844b87f77a5864e249f
fradkintal <fradkintal@gmail.com>
2022-12-12 19:11:54 +0200

    Update customer_model.py

--- models/customer_model.py
+++ models/customer_model.py
@@ -13,6 +13,7 @@ class CustomerModel(BaseModel):
     email: str | None = Field(default=None)
     address: str | None = Field(default=None)
     note: str | None = Field(default=None)
+    phone_book: dict | None = Field(default=None)
 
     class Config:
         allow_population_by_field_name = True
@@ -28,6 +29,7 @@ class CustomerModel(BaseModel):
                 "phone_number": "054-3532312",
                 "address": "123 Street, NYC",
                 "note": "screeming bitch",
+                "phone_book": {"name":"cfasdf","phone":"876543"}
             }
         }
 
@@ -39,6 +41,7 @@ class UpdateCustomerModel(BaseUpdateModel):
     email: str | None = Field(default=MISSING)
     address: str | None = Field(default=MISSING)
     note: str | None = Field(default=MISSING)
+    phone_book: dict | None = Field(default=MISSING)
 
     class Config:
         arbitrary_types_allowed = True
@@ -54,5 +57,6 @@ class UpdateCustomerModel(BaseUpdateModel):
                 "phone_number": "054-3532312",
                 "address": "123 Street, NYC",
                 "note": "screeming bitch",
+                "phone_book": {"name":"cfasdf","phone":"876543"}
             }
         }
