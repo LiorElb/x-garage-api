a587999cb4f457776c60a36fe6b4199f2cf2ddfd
fradkintal <fradkintal@gmail.com>
2022-12-13 23:49:21 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -217,10 +217,9 @@ async def get_car_types():
 @app.post("/cars", response_model=CarModel, status_code=HTTPStatus.CREATED, tags=['cars'])
 async def add_car(car: CarModel, bg_tasks: BackgroundTasks):
     car = jsonable_encoder(car)
-    # findcar = await CARS.find_one({"license_plate_number": car.license_plate_number})
-    # if findcar:
-    #     raise HTTPException(
-    #         status_code=404, detail=f"Car found")
+    findcar = await CARS.find_one({"license_plate_number": car.license_plate_number})
+    if findcar:
+        return "Car found"
     new = await CARS.insert_one(car)
     new_car = await CARS.find_one(
         {"_id": new.inserted_id},
